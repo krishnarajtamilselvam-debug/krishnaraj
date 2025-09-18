969af832b78e34d26c1c273694695b6aaf8b02a7
krishnaraj <krishnarajtamilselvam@gmail.com>
2025-09-18 12:49:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple form submission simulation
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  document.getElementById("formMsg").innerText = "Message sent successfully!";
+
+  this.reset();
+
+});
